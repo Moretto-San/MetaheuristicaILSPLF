3c86ab2226e2e40abee4393a8b75700c1e335f17
rafael.morett@gmail.com <rafael.morett@gmail.com>
2013-11-26 23:56:27 -0200

    Agora gerando Resultados.

--- Metaheuristica/src/Main.java
+++ Metaheuristica/src/Main.java
@@ -1,7 +1,17 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Random;
+
+import jxl.*;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
 
 public class Main {
 	public static String nomeIntancia;
@@ -9,50 +19,111 @@ public class Main {
 	public static Integer numCidades;
 	public static Integer[][] custoConexao;
 	public static Integer[] custoAberturaFacilidade;
+	static int linhaAtual = 0;
+	static int colunaAtual = 0;
+	static WritableWorkbook workbook1;
+	static WritableSheet s1;
 
-	public static void main(String[] args) {
-		for(int i=1;i<10;i++){
-			String nomeArq = "Instancias/B/B1."+i;
-			ils(nomeArq);	
+	public static void main(String[] args) throws RowsExceededException,
+			WriteException, IOException {
+		String filename1 = "Resultados.xls";
+		WorkbookSettings ws1 = new WorkbookSettings();
+		ws1.setLocale(new Locale("en", "EN"));
+		workbook1 = Workbook.createWorkbook(new File(filename1), ws1);
+		s1 = workbook1.createSheet("Resultados", 0);
+		Label label = new Label(colunaAtual, linhaAtual, "Resultados ILS");
+		s1.addCell(label);
+		linhaAtual++;
+		label = new Label(colunaAtual, linhaAtual, "Instancia");
+		s1.addCell(label);
+		colunaAtual++;
+		label = new Label(colunaAtual, linhaAtual, "Custo Otimo");
+		s1.addCell(label);
+		colunaAtual++;
+		label = new Label(colunaAtual, linhaAtual, "Custo ILS");
+		s1.addCell(label);
+		linhaAtual++;
+		colunaAtual=0;
+		for (int i = 1; i < 10; i++) {
+			String nomeArq = "Instancias/B/B1." + i;
+			ils(nomeArq);
 		}
-		for(int i=1;i<10;i++){
-			String nomeArq = "Instancias/C/C1."+i;
-			ils(nomeArq);	
+		for (int i = 1; i < 10; i++) {
+			String nomeArq = "Instancias/C/C1." + i;
+			ils(nomeArq);
 		}
-		for(int i=1;i<11;i++){
-			for(int j=1;j<11;j++){
-				String nomeArq = "Instancias/Dq/"+i+"/D"+i+"."+j;
+		for (int i = 1; i < 11; i++) {
+			for (int j = 1; j < 11; j++) {
+				String nomeArq = "Instancias/Dq/" + i + "/D" + i + "." + j;
 				ils(nomeArq);
-			}				
+			}
 		}
-		for(int i=1;i<11;i++){
-			for(int j=1;j<11;j++){
-				String nomeArq = "Instancias/Eq/"+i+"/E"+i+"."+j;
+		for (int i = 1; i < 11; i++) {
+			for (int j = 1; j < 11; j++) {
+				String nomeArq = "Instancias/Eq/" + i + "/E" + i + "." + j;
 				ils(nomeArq);
-			}				
+			}
 		}
+		workbook1.write();
+		workbook1.close();
+		System.err.println("Fim");
 	}
 
-	public static void ils(String nomeArq) {
-		leArqInstancia(nomeArq);
-		// TODO Auto-generated method stub
-		Random random = new Random();
-		random.setSeed(2);
-		Solucao corrente = new Solucao();
-		corrente.ConstruirSolucao(custoConexao, custoAberturaFacilidade, random);
-		corrente = buscaLocalRemoveFacilidade(corrente, random, custoConexao);
-		int iter = 0;
-		while (iter < 100) {
-			Solucao temp = corrente.copiarSolucao();
-			perturbacao(temp, random);
-			temp = buscaLocalRemoveFacilidade(temp, random, custoConexao);
-			if (temp.getCustoTotal() < corrente.getCustoTotal()) {
-				corrente = temp.copiarSolucao();
+	public static void ils(String nomeArq) throws RowsExceededException, WriteException {
+		int CustoMedio = 0;
+		Label label = new Label(colunaAtual, linhaAtual, "Resultados ILS");
+		for (int seed = 1; seed < 10; seed++) {
+			leArqInstancia(nomeArq);
+			Random random = new Random();
+			random.setSeed(seed);
+			Solucao corrente = new Solucao();
+			corrente.ConstruirSolucao(custoConexao, custoAberturaFacilidade,
+					random);
+			corrente = buscaLocalRemoveFacilidade(corrente, random,
+					custoConexao);
+			int iter = 0;
+			while (iter < 100) {
+				Solucao temp = corrente.copiarSolucao();
+				perturbacao(temp, random);
+				temp = buscaLocalRemoveFacilidade(temp, random, custoConexao);
+				if (temp.getCustoTotal() < corrente.getCustoTotal()) {
+					corrente = temp.copiarSolucao();
+				}
+				iter++;
 			}
-			iter++;
+			// System.err.println("Semente: " + seed);
+			// imprimiSolucao(corrente);
+			corrente.verificaNullclientesXFacilidade();
+			CustoMedio += corrente.getCustoTotal();
 		}
-		imprimiSolucao(corrente);
-		corrente.verificaNullclientesXFacilidade();
+		CustoMedio = CustoMedio / 9;
+		System.err.println("Instancia: " + nomeIntancia);
+		System.err.println("Numero de Facilidades: " + numFacilidades
+				+ " Numero de Cidades: " + numCidades);
+		System.err.println("Custo Medio: " + CustoMedio);
+		String nomeArqOpt = nomeArq+".opt";
+		String custoOtimo = "";
+		try {
+			RandomAccessFile instanciaArq = new RandomAccessFile(new File(
+					nomeArqOpt), "r");
+			String leitura = instanciaArq.readLine();
+			String[] split = leitura.split(" ");
+			custoOtimo =split[split.length - 1];
+			System.err.println("Custo Otimo: " + custoOtimo);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		label = new Label(colunaAtual, linhaAtual, nomeArq);
+		s1.addCell(label);
+		colunaAtual++;
+		label = new Label(colunaAtual, linhaAtual, custoOtimo);
+		s1.addCell(label);
+		colunaAtual++;
+		label = new Label(colunaAtual, linhaAtual, CustoMedio+"");
+		s1.addCell(label);
+		colunaAtual=0;
+		linhaAtual++;
 	}
 
 	public static void leArqInstancia(String nomeArq) {
@@ -60,7 +131,7 @@ public class Main {
 			RandomAccessFile instanciaArq = new RandomAccessFile(new File(
 					nomeArq), "r");
 			String leitura = instanciaArq.readLine();
-			nomeIntancia = leitura.split(" ")[1];
+			nomeIntancia = nomeArq;
 			leitura = instanciaArq.readLine();
 			numFacilidades = Integer.parseInt(leitura.split(" ")[0]);
 			numCidades = Integer.parseInt(leitura.split(" ")[1]);
@@ -101,12 +172,16 @@ public class Main {
 		while (melhorou) {
 			ArrayList<String> facilidadesUsadas = corrente
 					.getFacilidadesUsadas();
-			int aleatorio = random.nextInt(facilidadesUsadas.size() - 1);
-			temp = corrente.copiarSolucao();
-			temp.removeFacilidadesUsada(facilidadesUsadas.get(aleatorio));
-			temp.setCidadesSemFacilidade(custoConexao, custoAberturaFacilidade);
+			if (facilidadesUsadas.size() != 1) {
+				int aleatorio = random.nextInt(facilidadesUsadas.size() - 1);
+				temp = corrente.copiarSolucao();
+				temp.removeFacilidadesUsada(facilidadesUsadas.get(aleatorio));
+				temp.setCidadesSemFacilidade(custoConexao,
+						custoAberturaFacilidade);
+			}
 			if (temp.getCustoTotal() < corrente.getCustoTotal()) {
 				corrente = temp.copiarSolucao();
+
 			} else {
 				melhorou = false;
 			}
